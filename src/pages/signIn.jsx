aa1d81f3f3de1e40321e9cf67814b2446d871af0
Johnsavadkuhi <physitime@gmail.com>
2025-04-25 19:59:54 +0330

    update

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,42 +1,50 @@
-'use client';
-import { SignInPage } from '@toolpad/core/SignInPage';
-import { useNavigate } from 'react-router';
-import { useSession } from '../SessionContext';
+"use client";
+import { SignInPage } from "@toolpad/core/SignInPage";
+import { useNavigate } from "react-router";
+import { useSession } from "../SessionContext";
+import { loginAPI } from "../api/auth/loginAPI";
 
 const fakeAsyncGetSession = async (formData) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (formData.get('password') === 'password') {
-        resolve({
-          user: {
-            name: 'Bharat Kashyap',
-            email: formData.get('email') || '',
-            image: 'https://avatars.githubusercontent.com/u/19550456',
-          },
-        });
-      }
-      reject(new Error('Incorrect credentials.'));
-    }, 1000);
-  });
+  console.log("formData : ", formData.get("email"));
+
+  const username = formData.get("email");
+  const password = formData.get("password");
+
+  const data = await loginAPI(username, password);
+  console.log("data : ", data);
+
+  return {
+    user: {
+      name: data.user.username,
+      email: data.user.username,
+      image: "https://avatars.githubusercontent.com/u/19550456",
+    },
+  };
 };
 
 export default function SignIn() {
   const { setSession } = useSession();
   const navigate = useNavigate();
+
   return (
     <SignInPage
-      providers={[{ id: 'credentials', name: 'Credentials' }]}
+    
+      slotProps={{ emailField: { label: "Username", type: "text" } }}
+      providers={[{ id: "credentials", name: "username" }]}
       signIn={async (provider, formData, callbackUrl) => {
         // Demo session
         try {
           const session = await fakeAsyncGetSession(formData);
+          console.log("Session : ", session);
           if (session) {
             setSession(session);
-            navigate(callbackUrl || '/', { replace: true });
+            navigate(callbackUrl || "/", { replace: true });
             return {};
           }
         } catch (error) {
-          return { error: error instanceof Error ? error.message : 'An error occurred' };
+          return {
+            error: error instanceof Error ? error.message : "An error occurred",
+          };
         }
         return {};
       }}
