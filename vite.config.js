83d2907cc6a5853318d08c2dfba5b15b409dead8
Johnsavadkuhi <physitime@gmail.com>
2025-05-03 05:41:03 +0330

    use swc

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react() ,tailwindcss()],
+  plugins: [react({
+    babel: {
+      plugins: ['babel-plugin-react-compiler'],
+    },
+  }), tailwindcss()],
   server:{
     port:5173
   }, 
   preview:{
-    port:3002
+    port:5173
   }
 });
