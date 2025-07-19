0ba80bd6852cb793c3bad056953d9f76f8d02780
johnsavadkuhi <physitime@gmail.com>
2025-07-19 11:29:10 +0330

    update

---
+++ src/api/bugs/updateBugStatus.jsx
@@ -0,0 +1,11 @@
+import { apiFetch } from '../api';
+
+export const updateBugStatus = async (projectId , userId , bugId , status) => {
+    return apiFetch(`/api/projects/update/bug/status`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ projectId , userId , bugId , status }),
+    });
+  };
