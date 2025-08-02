543ea5dfe6842dc8fa7a3e4ec5e6e879fdf20632
Johnsavadkuhi <physitime@gmail.com>
2025-08-03 02:38:16 +0330

    update

--- src/api/projects/updateProjectStatus.jsx
+++ src/api/projects/updateProjectStatus.jsx
@@ -1,13 +1,15 @@
 import { apiFetch } from '../api';
 
-
-export const updateProjectStatus= async(projectId , newStatus   )=>{
-
-    return apiFetch('/api/projects/user/update/status' , {
-        method:"PUT" , 
-        body:JSON.stringify({projectId , newStatus  })
+export const updateProjectStatus = async (projectId, { newStatus, timeWorked = 0 }) => {
+  return apiFetch('/api/projects/user/update/status', {
+    method: "PUT",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ 
+      projectId, 
+      newStatus,
+      timeWorked // Include the calculated time worked
     })
-
-}
-
-
+  });
+};
