2bd09c1508b189d84ad8b9afb6def6764794edde
Johnsavadkuhi <physitime@gmail.com>
2025-08-07 05:51:24 +0330

    add

---
+++ src/report/components/PF11/data.jsx
@@ -0,0 +1,15 @@
+    export const data = [
+        { page: '', cvss: '', state: 'PASS', id: "4.2.1", description: '4.2.1- آزمون پیکربندی شبکه/زیرساخت', code: '(WSTG-CONF-01)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.2", description: '4.2.2- آزمون پیکربندی بستر برنامه‌ی کاربردی', code: '(WSTG-CONF-02)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.3", description: '4.2.3- آزمون مدیریت پسوند فایل ها برای داده‌های حساس', code: '(WSTG-CONF-03)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.4", description: '4.2.4- آزمون بررسی فایل های پشتیبان قدیمی و بی‌مرجع برای داده ‌های حساس', code: '(WSTG-CONF-04)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.5", description: '4.2.5- آزمون برشماری واسط‌های مدیریتی زیرساخت و برنامه‌', code: '(WSTG-CONF-05)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.6", description: '4.2.6- آزمون روش‌های HTTP', code: '(WSTG-CONF-06)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.7", description: '4.2.7- آزمون HSTS', code: '(WSTG-CONF-07)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.8", description: '4.2.8- آزمون سیاست‌های بین دامنه‌ای RIA', code: '(WSTG-CONF-08)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.9", description: '4.2.9- آزمون مجوز فایل', code: '(WSTG-CONF-09)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.10", description: '4.2.10- آزمون تصاحب زیردامنه', code: '(WSTG-CONF-10)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.11", description: '4.2.11- آزمون فضای ذخیره سازی ابری', code: '(WSTG-CONF-11)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.12", description: '4.2.12- آزمون CSP', code: '(WSTG-CONF-12)' },
+        { page: '', cvss: '', state: 'PASS', id: "4.2.13", description: '4.2.13- آزمون Path Confusion', code: '(WSTG-CONF-13)' }
+    ];
