faafddefc94f36db15cb7ac0cbafeb5bb80d7d92
Johnsavadkuhi <physitime@gmail.com>
2025-08-07 05:51:41 +0330

    update

--- src/report/utils/getMaxCvssValue.jsx
+++ src/report/utils/getMaxCvssValue.jsx
@@ -1,6 +1,6 @@
 export const getMaxCvssValue = (data) => {
     // Check if all `cvss` values are empty strings
-    const allEmpty = data.every(item => item.cvss === "");
+    const allEmpty = data?.every(item => item.cvss === "");
 
     // If all are empty, return -1
     if (allEmpty) {
@@ -8,10 +8,9 @@ export const getMaxCvssValue = (data) => {
     }
 
     // Extract `cvss` values, convert to numbers, and filter valid numbers
-    const cvssValues = data
-        .map(item => parseFloat(item.cvss)) // Convert to float
+    const cvssValues = data?.map(item => parseFloat(item.cvss)) // Convert to float
         .filter(value => !isNaN(value)); // Remove invalid numbers
 
     // Return the maximum value or -1 if there are no valid `cvss` values
-    return cvssValues.length > 0 ? Math.max(...cvssValues) : -1;
+    return cvssValues?.length > 0 ? Math.max(...cvssValues) : -1;
 };
