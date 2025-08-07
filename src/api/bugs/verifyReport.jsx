32f3db997be2e300b3f19494ea564669e6972d9e
Johnsavadkuhi <physitime@gmail.com>
2025-08-07 05:50:36 +0330

    update

--- src/api/bugs/verifyReport.jsx
+++ src/api/bugs/verifyReport.jsx
@@ -1,11 +1,24 @@
 import { apiFetch } from '../api';
 
-export const verifyReport = async (id , state ) => {
+// export const verifyReport = async (id , state ) => {
+//     return apiFetch(`/api/projects/user/report/verify`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ id , state  }),
+//     });
+//   };
+
+
+export const verifyReport = async (payload ) => {
+  console.log("payload in line 15 : " , payload)
+  
     return apiFetch(`/api/projects/user/report/verify`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ id , state  }),
+      body: JSON.stringify({ payload }),
     });
   };
