a044c6a25757c03c77288cfe1d6c73b326703884
Johnsavadkuhi <physitime@gmail.com>
2025-08-07 05:51:21 +0330

    update

--- src/report/components/PF/PF.jsx
+++ src/report/components/PF/PF.jsx
@@ -1,6 +1,6 @@
 import Poc from "../../pages/POC";
 
-function PF({ report, pageOffset }) {
+function PF({ reports, pageOffset }) {
   const colors = [
     { severity: "Info", color: "#00B0F0" },
     { severity: "Low", color: "#FFFF00" },
@@ -11,26 +11,25 @@ function PF({ report, pageOffset }) {
 
 
   return <>
-    {report && report.map((report, key) => {
-
+    {reports && reports?.map((report, key) => {
    const page = pageOffset + key  ; 
-if (report.severity === "Info")
+if (report.severity === "Info") 
 
         return <Poc rowColor={colors[0].color} key={report._id} report={report} page={page} />;
 
-      else if (report.severity === "Low") {
+      else if (report.severity === "LOW" || report.severity === "Low") {
 
         return <Poc textColor="#000000" rowColor={colors[1].color} key={report._id} report={report} page={page} />;
 
-      } else if (report.severity === "Medium") {
+      } else if (report.severity === "MEDIUM" || report.severity === "Medium") {
 
         return <Poc textColor="#000000" rowColor={colors[2].color} key={report._id} report={report} page={page} />;
 
-      } else if (report.severity === "High") {
+      } else if (report.severity === "HIGH" || report.severity === "High") {
 
         return <Poc rowColor={colors[3].color} key={report._id} report={report} page={page} />;
 
-      } else if (report.severity === "Critical") {
+      } else if (report.severity === "CRITICAL" || report.severity === "Critical") {
 
         return <Poc rowColor={colors[4].color} key={report._id} report={report} page={page} />;
 
