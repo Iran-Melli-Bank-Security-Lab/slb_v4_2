156d81e5694347b7ef70a0c98ff62ee182c30731
Johnsavadkuhi <physitime@gmail.com>
2025-08-08 11:22:19 +0330

    update

--- src/socket.js
+++ src/socket.js
@@ -1,13 +1,14 @@
-// src/socket.js
 import { io } from 'socket.io-client';
 
 const URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
 
 export const socket = io(URL, {
-  autoConnect: false,        // manual connect()
-  transports: ['websocket'], // optional: skip polling
- method:["GET" , "POST"], 
-  withCredentials: true,     // ← send cookies on handshake
-  
+  autoConnect: false,
+  transports: ['websocket'],
+  withCredentials: true,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 2000,
+  reconnectionDelayMax: 10000,
+  timeout: 20000,
 });
- 
