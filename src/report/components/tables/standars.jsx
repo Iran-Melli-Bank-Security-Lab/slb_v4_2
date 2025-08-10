47976efc9b2a34fefa52042f711845ce5c9067d9
Johnsavadkuhi <physitime@gmail.com>
2025-08-10 12:45:47 +0330

    update

---
+++ src/report/components/tables/standars.jsx
@@ -0,0 +1,227 @@
+export const standard = {
+  "mappings": [
+    {
+        "id":"4.4.8",
+      "iso_15408_component": "FAU_GEN",
+      "owasp_wstg_category": "WSTG-ATHN-08",
+      "description": "Ensures security-relevant events are logged."
+    },
+    {
+        "id":"4.4.8",
+      "iso_15408_component": "FAU_STG",
+      "owasp_wstg_category": "WSTG-ATHN-08",
+      "description": "Ensures logs are protected from unauthorized access/modification."
+    },
+    {
+        "id":"4.1.2", 
+      "iso_15408_component": "FAU_SAR",
+      "owasp_wstg_category": "WSTG-INFO-02",
+      "description": "Ensures logs are readable and interpretable."
+    },
+    {
+        "id":"4.4.8",
+      "iso_15408_component": "FAU_SEL",
+      "owasp_wstg_category": "WSTG-ATHN-08",
+      "description": "Ensures selective audit events are captured."
+    },
+    {
+        "id":"4.9.3",
+      "iso_15408_component": "FCS_CKM",
+      "owasp_wstg_category": "WSTG-CRYP-03",
+      "description": "Ensures proper key generation, storage, and destruction."
+    },
+    {
+        "id":"4.9.1",
+      "iso_15408_component": "FCS_COP",
+      "owasp_wstg_category": "WSTG-CRYP-01",
+      "description": "Ensures proper encryption/decryption operations."
+    },
+    {
+        "id":"4.9.1",
+      "iso_15408_component": "FCS-TLSS_EXT",
+      "owasp_wstg_category": "WSTG-CRYP-01",
+      "description": "Ensures secure TLS server configuration."
+    },
+    {
+        "id":"4.9.1",
+      "iso_15408_component": "FCS-TLSC_EXT",
+      "owasp_wstg_category": "WSTG-CRYP-01",
+      "description": "Ensures secure TLS client configuration."
+    },
+    {
+        "id":"4.4.3",
+      "iso_15408_component": "FIA_AFL",
+      "owasp_wstg_category": "WSTG-ATHN-03",
+      "description": "Ensures account lockout after failed attempts."
+    },
+    {
+        "id":"4.4.2",
+      "iso_15408_component": "FIA_PMG_EXT",
+      "owasp_wstg_category": "WSTG-ATHN-02",
+      "description": "Ensures strong password policies."
+    },
+    {
+        "id":"4.4.1",
+      "iso_15408_component": "FIA_UAU",
+      "owasp_wstg_category": "WSTG-ATHN-01",
+      "description": "Ensures secure authentication mechanisms."
+    },
+    {
+        "id":"4.3.1",
+      "iso_15408_component": "FIA_ATD",
+      "owasp_wstg_category": "WSTG-IDNT-01",
+      "description": "Ensures proper user attribute assignment."
+    },
+    {
+        "id":"4.3.2",
+      "iso_15408_component": "FIA_UID",
+      "owasp_wstg_category": "WSTG-IDNT-02",
+      "description": "Ensures unique user identification."
+    },
+    {
+        "id":"4.6.4",
+      "iso_15408_component": "FIA_USB",
+      "owasp_wstg_category": "WSTG-SESS-04",
+      "description": "Ensures proper user-session binding."
+    },
+    {
+        "id":"4.2.4",
+      "iso_15408_component": "FTP_TRP",
+      "owasp_wstg_category": "WSTG-CONF-04",
+      "description": "Ensures secure communication channels."
+    },
+    {
+        "id":"4.10.9",
+      "iso_15408_component": "FRU_FLT",
+      "owasp_wstg_category": "WSTG-BUSL-09",
+      "description": "Ensures system recovers from failures."
+    },
+  
+    {
+        "id":"4.2.1",
+      "iso_15408_component": "FDP_ITC",
+      "owasp_wstg_category": "WSTG-CONF-01",
+      "description": "Ensures secure data exchange between systems."
+    },
+    {
+        "id":"4.2.5",
+      "iso_15408_component": "FMT_MOF",
+      "owasp_wstg_category": "WSTG-CONF-05",
+      "description": "Ensures secure function management."
+    },
+    {
+        "id":"4.2.5",
+      "iso_15408_component": "FMT_MTD",
+      "owasp_wstg_category": "WSTG-CONF-05",
+      "description": "Ensures secure handling of security data."
+    },
+    {
+        "id":"4.2.5",
+      "iso_15408_component": "FMT_SMF",
+      "owasp_wstg_category": "WSTG-CONF-05",
+      "description": "Ensures proper security function specs."
+    },
+    {
+        "id":"4.3.1",
+      "iso_15408_component": "FMT_SMR",
+      "owasp_wstg_category": "WSTG-IDNT-01",
+      "description": "Ensures proper role-based access control."
+    },
+  
+    {
+        "id":"4.10.8",
+      "iso_15408_component": "FPT_TUD_EXT",
+      "owasp_wstg_category": "WSTG-BUSL-08",
+      "description": "Ensures secure time/date handling."
+    },
+    {
+        "id":"4.8.1",
+      "iso_15408_component": "FPT_FLS",
+      "owasp_wstg_category": "WSTG-ERRH-01",
+      "description": "Ensures system fails securely."
+    },
+    
+    {
+        "id":"4.2.4",
+      "iso_15408_component": "FPT_ITT",
+      "owasp_wstg_category": "WSTG-CONF-04",
+      "description": "Ensures secure internal data transfers."
+    },
+    {
+        "id":"4.10.8",
+      "iso_15408_component": "FPT_STM",
+      "owasp_wstg_category": "WSTG-BUSL-08",
+      "description": "Ensures secure timestamp handling."
+    },
+    {
+        "id":"4.6.3",
+      "iso_15408_component": "FTA_SSL",
+      "owasp_wstg_category": "WSTG-SESS-03",
+      "description": "Ensures session locks after inactivity."
+    },
+    {
+        "id":"4.6.2",
+      "iso_15408_component": "FTA_MCS",
+      "owasp_wstg_category": "WSTG-SESS-02",
+      "description": "Ensures control over multiple sessions."
+    },
+    {
+        "id":"4.4.8",
+      "iso_15408_component": "FTA_TAH",
+      "owasp_wstg_category": "WSTG-ATHN-08",
+      "description": "Ensures session history tracking."
+    },
+    {
+        "id":"4.6.1",
+      "iso_15408_component": "FTA_TSE",
+      "owasp_wstg_category": "WSTG-SESS-01",
+      "description": "Ensures secure session establishment."
+    },
+    {
+        "id":"4.2.1",
+      "iso_15408_component": "FDP_ETC",
+      "owasp_wstg_category": "WSTG-CONF-01",
+      "description": "Ensures secure data export."
+    },
+   
+   
+ 
+    {
+        "id":"4.9.1",
+      "iso_15408_component": "FCS_HTTPS_EXT",
+      "owasp_wstg_category": "WSTG-CRYP-01",
+      "description": "Ensures secure HTTPS configuration."
+    },
+    {
+        "id":"4.9.1",
+      "iso_15408_component": "FCS-DTLS_EXT",
+      "owasp_wstg_category": "WSTG-CRYP-01",
+      "description": "Ensures secure DTLS configuration."
+    },
+    {
+      "id":"4.9.2",
+      "iso_15408_component": "FCS_X509_EXT",
+      "owasp_wstg_category": "WSTG-CRYP-02",
+      "description": "Ensures proper X.509 certificate handling."
+    },
+    {
+        "id":"4.12.1",
+      "iso_15408_component": "FPT_API_EXT",
+      "owasp_wstg_category": "WSTG-APIT-01",
+      "description": "Ensures secure API interactions."
+    },
+    {
+        "id":"4.10.7",
+      "iso_15408_component": "FPT_AEX_EXT",
+      "owasp_wstg_category": "WSTG-BUSL-07",
+      "description": "Ensures anti-exploitation mechanisms."
+    },
+    {
+        "id":"4.1.8",
+      "iso_15408_component": "FPT_LIB_EXT",
+      "owasp_wstg_category": "WSTG-INFO-08",
+      "description": "Ensures secure library usage."
+    }
+  ],
+
+}
