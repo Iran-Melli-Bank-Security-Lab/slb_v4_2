234ea4bd6dfb6e1557324096d57966a6d2829d56
Johnsavadkuhi <physitime@gmail.com>
2025-08-10 12:45:44 +0330

    update

--- src/report/components/tables/ReportTestSecond.jsx
+++ src/report/components/tables/ReportTestSecond.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import "./document.css";
 import Tr from './Tr';
 
-const ReportTestSecond = ({count}) => {
+const ReportTestSecond = ({count , data }) => {
 
     // Function to determine test prop values based on count
     const getTestValues = (count) => {
-        return {
-            test1: true,
-            test2: count >= 2,
-            test3: count >= 3,
-            test4: count >= 4
+        // return {
+        //     test1: true,
+        //     test2: count >= 2,
+        //     test3: count >= 3,
+        //     test4: count >= 4
+        // };
+         return {
+            test1: false ,
+            test2: false ,
+            test3: false ,
+            test4: true 
         };
     };
 
@@ -20,111 +26,111 @@ const ReportTestSecond = ({count}) => {
             cellSpacing="0"
         >
             <tbody>
-                <Tr {...getTestValues(count)}>
+                <Tr label="FRU_FLT" {...getTestValues(count)} data={data}>
                     FRU_FLT
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FDP_RIP" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FDP_RIP
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FDP_ITC" {...getTestValues(count)}>
                     FDP_ITC
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FMT_MOF" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FMT_MOF
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FMT_MTD" {...getTestValues(count)} data={data}>
                     FMT_MTD
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FMT_SMF" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FMT_SMF
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FMT_SMR" {...getTestValues(count)} data={data} >
                     FMT_SMR
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FMT_MSA" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FMT_MSA
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FPT_TUD_EXT" {...getTestValues(count)} data={data}>
                     FPT_TUD_EXT
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FPT_FLS" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FPT_FLS
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FPT_TDC" {...getTestValues(count)} data={data}>
                     FPT_TDC
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FPT_ITT" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FPT_ITT
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FPT_STM" {...getTestValues(count)} data={data}>
                     FPT_STM
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FTA_SSL" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FTA_SSL
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FTA_MCS" {...getTestValues(count)} data={data}>
                     FTA_MCS
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FTA_TAH" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FTA_TAH
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FTA_TSE" {...getTestValues(count)} data={data}>
                     FTA_TSE
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr  label="FDP_ETC" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FDP_ETC
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FDP_SDI" {...getTestValues(count)} data={data}>
                     FDP_SDI
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FDP_ACC" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FDP_ACC
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FDP_ACF" {...getTestValues(count)} data={data}>
                     FDP_ACF
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FCS_HTTPS_EXT" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FCS_HTTPS_EXT
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FCS-DTLS_EXT" {...getTestValues(count)} data={data}>
                     FCS-DTLS_EXT
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr  label="FCS_X509_EXT" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FCS_X509_EXT
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FPT_API_EXT" {...getTestValues(count)} data={data}>
                     FPT_API_EXT
                 </Tr>
 
-                <Tr {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr  label="FPT_AEX_EXT" {...getTestValues(count)} bgcolor="#D7D7D7" data={data}>
                     FPT_AEX_EXT
                 </Tr>
 
-                <Tr {...getTestValues(count)}>
+                <Tr label="FPT_LIB_EXT" {...getTestValues(count)} data={data}>
                     FPT_LIB_EXT
                 </Tr>
 
