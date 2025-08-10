2a08c3c018c0ae61efb7c5fc3c16f67c35ddecba
Johnsavadkuhi <physitime@gmail.com>
2025-08-10 12:45:41 +0330

    update

--- src/report/components/tables/ReportTest.jsx
+++ src/report/components/tables/ReportTest.jsx
@@ -31,63 +31,63 @@ const ReportTest = ({count , data }) => {
                     عنوان الزام
                 </TestOrder>
 
-                <Tr data={data} {...getTestValues(count)}>
+                <Tr label="FAU_GEN" data={data} {...getTestValues(count)}>
                     FAU_GEN
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FAU_STG" data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
                     FAU_STG
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)}>
+                <Tr label="FAU_SAR" data={data} {...getTestValues(count)}>
                     FAU_SAR
                 </Tr>
 
-                <Tr  data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr  label="FAU_SEL" data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
                     FAU_SEL
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)}>
+                <Tr label="FCS_CKM" data={data} {...getTestValues(count)}>
                     FCS_CKM
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FCS_COP" data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
                     FCS_COP
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)}>
+                <Tr label="FCS-TLSS_EXT" data={data} {...getTestValues(count)}>
                     FCS-TLSS_EXT
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FCS-TLSC_EXT" data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
                     FCS-TLSC_EXT
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)}>
+                <Tr label="FIA_AFL" data={data} {...getTestValues(count)}>
                     FIA_AFL
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FIA_PMG_EXT" data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
                     FIA_PMG_EXT
                 </Tr>
 
-                <Tr  data={data} {...getTestValues(count)}>
+                <Tr label="FIA_UAU" data={data} {...getTestValues(count)}>
                     FIA_UAU
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FIA_ATD" data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
                     FIA_ATD
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)}>
+                <Tr label="FIA_UID" data={data} {...getTestValues(count)}>
                     FIA_UID
                 </Tr>
 
-                <Tr data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
+                <Tr label="FIA_USB" data={data} {...getTestValues(count)} bgcolor="#D7D7D7">
                     FIA_USB
                 </Tr>
 
-                <Tr  data={data} {...getTestValues(count)}>
+                <Tr label="FTP_TRP"  data={data} {...getTestValues(count)}>
                     FTP_TRP
                 </Tr>
             </tbody>
