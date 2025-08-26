8fa8f9b4f837910467d76ede4bcca966213d3822
Johnsavadkuhi <physitime@gmail.com>
2025-08-26 12:00:33 +0330

    update

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -5,35 +5,35 @@ import { useSession } from "../SessionContext";
 import { loginAPI } from "../api/auth/loginAPI";
 
 const fakeAsyncGetSession = async (formData) => {
-  console.log("formData line 8 in signIn: ", formData.get("email") );
+  console.log("formData line 8 in signIn: ", formData.get("email"));
 
   const username = formData.get("email");
   const password = formData.get("password");
 
   const data = await loginAPI(username, password);
-  console.log("data in line 14 : ", data);
 
-const plainUser = (typeof data.user.toJSON === "function" ? data.user.toJSON() : data.user);
+  const plainUser = (typeof data.user.toJSON === "function" ? data.user.toJSON() : data.user);
 
-// const plainUser = data.user._doc;
+  // const plainUser = data.user._doc;
 
   const result = {
-  user: {
-    id: plainUser.id , 
-    name: plainUser.username , 
-    email: plainUser.username ,
-    firstName: plainUser.firstName, 
-    lastName: plainUser.lastName, 
-    roles: plainUser.roles, 
-    image: plainUser.image, 
-    score: plainUser.score, 
-    devOps: plainUser.devOps, 
-    userProject: plainUser.userProject,
-  }
-};
+    token: data?.token,
+    user: {
+      id: plainUser.id,
+      name: plainUser.username,
+      email: plainUser.username,
+      firstName: plainUser.firstName,
+      lastName: plainUser.lastName,
+      roles: plainUser.roles,
+      image: plainUser.image,
+      score: plainUser.score,
+      devOps: plainUser.devOps,
+      userProject: plainUser.userProject,
+    }
+  };
 
 
-return result ; 
+  return result;
 };
 
 export default function SignIn() {
@@ -42,7 +42,7 @@ export default function SignIn() {
 
   return (
     <SignInPage
-    
+
       slotProps={{ emailField: { label: "Username", type: "text" } }}
       providers={[{ id: "credentials", name: "username" }]}
       signIn={async (provider, formData, callbackUrl) => {
