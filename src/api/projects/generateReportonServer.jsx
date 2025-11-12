aa37cb0bd74d4c3a0504bf514ca5a32e795ad563
Johnsavadkuhi <physitime@gmail.com>
2025-11-12 14:30:28 +0330

    update

--- src/api/projects/generateReportonServer.jsx
+++ src/api/projects/generateReportonServer.jsx
@@ -1,12 +1,14 @@
 import { apiFetch } from '../api';
 
-export const generateReportonServer = async (url ) => {
+export const generateReportonServer = async (url , projectId  ) => {
+
+  console.log("url in line 5 at generate report on server : " , url )
     // choose your base path
     const basePath = "/api/projects/generateReport"
         
   
     // build a query string
-    const params = new URLSearchParams({ url });
+    const params = new URLSearchParams({ url , projectId  });
   
     // call apiFetch with the full URL
     return  apiFetch(`${basePath}?${params.toString()}`, {
